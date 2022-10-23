20436c323412a7518c2f573f3a482da48fb3b2f6
gunther <korzhovdh@gmail.com>
2022-10-23 18:42:49 +0300

    mailer not work

--- src/js/script.js
+++ src/js/script.js
@@ -89,5 +89,30 @@ $(document).ready(function(){
 
       $('input[name=phone]').mask("+8(999) 99-99-999");
 
+
+
+      $('form').submit(function(e){
+          e.preventDefault();
+
+          if (!$(this).valid()) {
+            return;
+          }
+
+          $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+          }).done(function() {
+              $(this).find("input").val("");
+              $('#consultation, #order').fadeOut();
+              $('.overlay, #thanks').fadeIn(slow);
+
+
+
+              $('form').trigger('reset');
+          });
+          return false;
+      });
+
   });
           
