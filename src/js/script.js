f0d0ae1c0d56ad0a1bef4686554d02426b87bdca
gunther <korzhovdh@gmail.com>
2022-10-26 18:54:30 +0300

    validation

--- src/js/script.js
+++ src/js/script.js
@@ -126,7 +126,7 @@ $(document).ready(function(){
       });
 
 
-      $("a[href^='3']").click(function(){
+      $("a[href=#up]").click(function(){
           const _href = $(this).attr("href");
           $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
           return false;
